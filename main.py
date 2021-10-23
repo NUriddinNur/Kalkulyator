a03004eba7f644412e5ec9fb4439faed742e06e1
Nuriddin <Nurik900707@gmail.com>
2021-10-23 08:42:24 +0500

    MainWindow

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print("salom")
-print("hello")
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Kalkulyator")
+
+        self.setCentralWidget()
+
+
+
+app = QApplication([])
+
+window = QMainWindow()
+window.show()
+
+
+app.exec()
