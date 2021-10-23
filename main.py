1d1180d9836068b5255c23648e3a1444767e5c1e
Nuriddin <Nurik900707@gmail.com>
2021-10-23 09:30:40 +0500

    box1

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
-
+from PyQt5.QtWidgets import QApplication, QLineEdit, QHBoxLayout, QWidget, QMainWindow, QPushButton, QLabel, QVBoxLayout
+from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -7,14 +7,27 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("Kalkulyator")
 
-        self.setCentralWidget()
+        self.mainBox = QVBoxLayout()
+        self.mainBox.setAlignment(Qt.AlignTop)
+
+        self.Box1 = QHBoxLayout()
+        self.input = QLineEdit()
+        self.Box1.addWidget(self.input)
+
+        self.mainBox.addLayout(self.Box1)
+
+        container = QWidget()
+        container.setLayout(self.mainBox)
+
+        self.setCentralWidget(container)
+
 
 
 
 app = QApplication([])
 
-window = QMainWindow()
+window = MainWindow()
 window.show()
 
-
 app.exec()
+
