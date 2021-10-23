a865475ed6e2923acced937a34f5654323df61aa
Nuriddin <Nurik900707@gmail.com>
2021-10-23 09:40:15 +0500

    Box2

--- main.py
+++ main.py
@@ -10,11 +10,26 @@ class MainWindow(QMainWindow):
         self.mainBox = QVBoxLayout()
         self.mainBox.setAlignment(Qt.AlignTop)
 
+        # Box1
         self.Box1 = QHBoxLayout()
         self.input = QLineEdit()
         self.Box1.addWidget(self.input)
 
+        # Box2
+        self.Box2 = QHBoxLayout()
+        self.c_button = QPushButton("C")
+        self.minus_button = QPushButton("+/-")
+        self.divide_button = QPushButton("/")
+        self.backspace_button = QPushButton("<--")
+        self.Box2.addWidget(self.c_button)
+        self.Box2.addWidget(self.minus_button)
+        self.Box2.addWidget(self.divide_button)
+        self.Box2.addWidget(self.backspace_button)
+
+
+
         self.mainBox.addLayout(self.Box1)
+        self.mainBox.addLayout(self.Box2)
 
         container = QWidget()
         container.setLayout(self.mainBox)
